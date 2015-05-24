6abc52c14fb3b7f5da1e718122b7561391a40da0
Technius <techniux@gmail.com>
2015-05-23 19:32:58 -0700

    Fix html serving

--- src/main/scala/org/vocabstar/Routes.scala
+++ src/main/scala/org/vocabstar/Routes.scala
@@ -3,11 +3,18 @@ package org.vocabstar
 import akka.actor.ActorSystem
 import akka.stream.ActorFlowMaterializer
 import akka.http.scaladsl._
+import akka.http.scaladsl.marshalling.{ PredefinedToEntityMarshallers, ToEntityMarshaller }
+import akka.http.scaladsl.model.{ HttpEntity, MediaTypes, ResponseEntity }
 import akka.http.scaladsl.server.Directives._
 import play.twirl.api.Html
 
 class Routes(implicit system: ActorSystem, mat: ActorFlowMaterializer)
     extends util.Json4sMarshalling {
+
+  val htmlMarshaller: ToEntityMarshaller[Html] =
+    PredefinedToEntityMarshallers
+      .stringMarshaller(MediaTypes.`text/html`)
+      .compose(_.body)
 
   val default =
     path("api" / "words")(wordsApi) ~
@@ -18,11 +25,12 @@ class Routes(implicit system: ActorSystem, mat: ActorFlowMaterializer)
       }
     }
 
-  def displayPage(page: Html) = encodeResponse { 
-    complete {
-      page.body
+  def displayPage(page: Html) =
+    encodeResponse {
+      completeWith(htmlMarshaller) { callback =>
+        callback(page)
+      }
     }
-  }
 
   val wordsApi =
     get {
