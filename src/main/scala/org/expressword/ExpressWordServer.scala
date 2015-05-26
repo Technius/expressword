beb618f7429d5d4f351f463d5ccabd856b0d3826
Technius <techniux@gmail.com>
2015-05-25 17:19:03 -0700

    Implement logging

--- src/main/scala/org/expressword/ExpressWordServer.scala
+++ src/main/scala/org/expressword/ExpressWordServer.scala
@@ -1,14 +1,17 @@
 package org.expressword
 
 import akka.actor.{ ActorSystem, Props }
+import akka.event.Logging
 import akka.stream.ActorFlowMaterializer
 import akka.http.scaladsl.Http
 import com.typesafe.config.ConfigFactory
+import scala.util.Success
 
 object ExpressWordServer extends App {
 
   implicit val system = ActorSystem("expressword")
   implicit val materializer = ActorFlowMaterializer()
+  implicit val logger = Logging(system, getClass)
 
   val config = ConfigFactory.load()
   val host = config.getString("host")
@@ -20,5 +23,15 @@ object ExpressWordServer extends App {
   val searchService = new service.SearchService(googleApiKey, googleCseId)
   val routes = new Routes(wordService, searchService)
 
+  import system.dispatcher
+
+  logger.info("Attempting to start server...")
   val serverFuture = Http().bindAndHandle(routes.default, host, port)
+  serverFuture onComplete {
+    case Success(_) =>
+      logger.info("Server started")
+    case _ =>
+      logger.error("Server failed to start")
+      system.shutdown()
+  }
 }
--- src/main/scala/org/expressword/Routes.scala
+++ src/main/scala/org/expressword/Routes.scala
@@ -1,6 +1,7 @@
 package org.expressword
 
 import akka.actor.{ ActorRef, ActorSystem }
+import akka.event.LoggingAdapter
 import akka.pattern.ask
 import akka.stream.ActorFlowMaterializer
 import akka.http.scaladsl._
@@ -8,14 +9,15 @@ import akka.http.scaladsl.model.MediaTypes
 import akka.http.scaladsl.server.Directives._
 import akka.util.Timeout
 import scala.concurrent.duration._
-import scala.util.Failure
+import scala.util.{ Failure, Success }
 
 import WordCategory._
 import service._
 import service.WordService._
 
 class Routes(WordService: ActorRef, SearchService: SearchService)
-            (implicit system: ActorSystem, mat: ActorFlowMaterializer)
+              (implicit system: ActorSystem, mat: ActorFlowMaterializer,
+                logging: LoggingAdapter)
     extends util.Json4sMarshalling {
 
   import system.dispatcher
@@ -23,48 +25,57 @@ class Routes(WordService: ActorRef, SearchService: SearchService)
   implicit val timeout: Timeout = 5.seconds
 
   val default =
-    encodeResponse {
-      path("api" / "words") {
-        (get & parameter("search")) { query =>
-          complete {
-            (WordService ? SearchWord(query)).mapTo[Seq[Vocabulary]].map {
-              case Seq() => ApiResponse.failure("No results found.")
-              case r => ApiResponse.success(r)
+    logRequestResult("expressword-server") {
+      encodeResponse {
+        path("api" / "words") {
+          (get & parameter("search")) { query =>
+            complete {
+              (WordService ? SearchWord(query)).mapTo[Seq[Vocabulary]].map {
+                case Seq() => ApiResponse.failure("No results found.")
+                case r => ApiResponse.success(r)
+              }
+            }
+          } ~
+          (post & entity(as[Vocabulary])) { vocab =>
+            complete {
+              (WordService ? UpdateWord(vocab)).mapTo[Option[Vocabulary]] map {
+                case Some(old) => ApiResponse.success(old)
+                case _ =>
+                  logging.debug(s"Scraping word data for '${vocab.word}'")
+                  SearchService.scrapeData(vocab) onComplete {
+                    case Success(updated) =>
+                      logging.debug(
+                        s"Updating information for '${vocab.word}': $updated")
+                      WordService ! UpdateWord(updated)
+                    case Failure(err) =>
+                      logging.error(
+                        s"Failed to retrieve word data for '${vocab.word}'")
+                  }
+                  ApiResponse.success("Word added")
+              }
             }
           }
         } ~
-        (post & entity(as[Vocabulary])) { vocab =>
-          complete {
-            (WordService ? UpdateWord(vocab)).mapTo[Option[Vocabulary]] map {
-              case Some(old) => ApiResponse.success(old)
-              case _ =>
-                SearchService.scrapeData(vocab) onSuccess {
-                  case updated => WordService ! UpdateWord(updated)
-                }
-                ApiResponse.success("Word added")
+        path("api" / "words" / Segment) { word =>
+          get {
+            complete {
+              (WordService ? FindWordExact(word))
+                .mapTo[Option[Vocabulary]]
+                .map(ApiResponse.fromOpt(_, "Not found"))
             }
-          }
-        }
-      } ~
-      path("api" / "words" / Segment) { word =>
-        get {
-          complete {
-            (WordService ? FindWordExact(word)).mapTo[Option[Vocabulary]] map {
-              ApiResponse.fromOpt(_, "Not found")
+          } ~
+          delete {
+            complete {
+              (WordService ? RemoveWord(word)).mapTo[Option[Vocabulary]] map {
+                ApiResponse.fromOpt(_, "Not found")
+              }
             }
           }
         } ~
-        delete {
-          complete {
-            (WordService ? RemoveWord(word)).mapTo[Option[Vocabulary]] map {
-              ApiResponse.fromOpt(_, "Not found")
-            }
-          }
-        }
-      } ~
-      path("assets" / Rest) { path =>
-        getFromResource(s"assets/$path")
-      } ~
-      getFromResource("assets/template.html")
+        path("assets" / Rest) { path =>
+          getFromResource(s"assets/$path")
+        } ~
+        getFromResource("assets/template.html")
+      }
     }
 }
